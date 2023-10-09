03928bbf11048b1061fe8561d34361aa0d950b7f
Maria Clara Souza <clarasouza1995@gmail.com>
2023-10-08 22:24:58 -0300

    adc md
    adc mudancas

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SenhasService {
-  // Variáveis públicas para armazenar contagens de senhas
+  public inputNovaSenha: string = '';
   public senhasGeral: number = 0;
   public senhasPrior: number = 0;
   public senhasExame: number = 0;
@@ -28,5 +28,46 @@ export class SenhasService {
   somaExame() {
     this.senhasExame++;
     this.senhasTotal++;
+  }
+
+  // Inicialize senhasArray como um objeto vazio
+  public senhasArray: { [key: string]: string[] } = {
+    'SG': [],
+    'SP': [],
+    'SE': [],
+  };
+
+  novaSenha(tipoSenha: string = '') {
+    if (tipoSenha === 'SG') {
+      this.somaGeral();
+      this.inputNovaSenha =
+        new Date().getFullYear().toString().substring(2, 4) +
+        new Date().getMonth().toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SG'].length + 1).toString().padStart(2, '0');
+    } else if (tipoSenha === 'SP') {
+      this.somaPrior();
+      this.inputNovaSenha =
+        new Date().getFullYear().toString().substring(2, 4) +
+        new Date().getMonth().toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SP'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SP'].push(this.inputNovaSenha);
+    } else if (tipoSenha === 'SE') {
+      this.somaExame();
+      this.inputNovaSenha =
+        new Date().getFullYear().toString().substring(2, 4) +
+        new Date().getMonth().toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SE'].push(this.inputNovaSenha);
+    }
+    console.log(this.senhasArray);
   }
 }
